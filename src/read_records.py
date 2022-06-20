38c43673b0032d7cb1f31b166d30b040aa38050e
franciscopastorimata <fjpastorimata@gmail.com>
2022-06-20 10:52:17 -0300

    implement tf records and visualize some images

---
+++ src/read_records.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import os
+
+TRAIN_FULL_TF_RECORDS_PATH = "./../data/retinopatia_entrenamiento/tf_records"
+
+# Create a dictionary describing the features.
+image_feature_description = {
+    'image': tf.io.FixedLenFeature([], tf.string),
+    'image_name': tf.io.FixedLenFeature([], tf.string),
+    'eye': tf.io.FixedLenFeature([], tf.int64),
+    'level_cat': tf.io.FixedLenFeature([], tf.int64),
+}
+
+def read_full_tf_training_records():
+    return tf.data.TFRecordDataset(
+        [f"{TRAIN_FULL_TF_RECORDS_PATH}/{tf_record_name}"
+         for tf_record_name in os.listdir(TRAIN_FULL_TF_RECORDS_PATH)])
+
+def _parse_image_function(example_proto):
+  # Parse the input tf.train.Example proto using the dictionary above.
+  return tf.io.parse_single_example(example_proto, image_feature_description)
+
+if __name__ == '__main__':
+    pass
