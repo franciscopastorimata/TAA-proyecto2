11c3a2c742baa018d263b4b8d9b2df6cd893f6aa
Federico Peluffo <fede@Federicos-MBP.guest.fing.edu.uy>
2022-06-29 10:10:28 -0300

    convert test to tfrecord

--- src/create_test.py
+++ src/create_test.py
@@ -1,15 +1,20 @@
 import os
-folder_path = "./../data/retinopatia_entrenamiento"
+import pandas as pd
+from natsort import natsorted
+
+folder_path = "./../data/retinopatia_entrenamiento/"
 
 if __name__ == '__main__':
     os.listdir(folder_path)
     test_img = os.path.join(folder_path, os.listdir(folder_path)[3])
     test_images = os.listdir(test_img)
+
     images = []
     for i in test_images:
         name = i.split('.')[0]
         images.append(name)
-    df = pd.DataFrame({'image':images })
+        
+    img = natsorted(images)
+    df = pd.DataFrame({'image': img})
 
-    df.to_csv('dataTest.csv')
-    cp dataTest.csv "./../data/retinopatia_entrenamiento"
+    df.to_csv('./../data/retinopatia_entrenamiento/dataTest.csv', index=False)
